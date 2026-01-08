1672976384ff5f26a6d2d87ec040cd5d56646313
Bernhard Glück <bernhard.glueck@aitive.at>
2026-01-08 04:05:20 +0100

    Introduces pipeline builder interface
    Defines an interface for pipeline builders to facilitate
    more flexible and testable pipeline construction.
    
    This allows for improved extensibility and separation
    of concerns within the pipeline building process.

---
+++ src/Aitive.Framework/Functional/Pipelines/IPipelineBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace Aitive.Framework.Functional.Pipelines;
+
+public interface IPipelineBuilder<T, TStep, TPhase, TSelf>
+    where TStep : IPipelineStep<T>
+    where TSelf : IPipelineBuilder<T, TStep, TPhase, TSelf>
+{
+    TSelf AddStep(TPhase phase, TStep step, int order = 0);
+}
