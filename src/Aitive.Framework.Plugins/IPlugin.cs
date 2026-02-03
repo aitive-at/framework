f567d28301def608a4c9407799ac70ae9d2182de
Bernhard Glück <bernhard.glueck@aitive.at>
2026-02-03 13:01:17 +0100

    Adds plugin querying capability
    Adds the ability for plugins to be queried for implementations of specific interfaces.
    It is now the caller's responsibility to dispose of disposable or async disposable instances.

--- src/Aitive.Framework.Plugins/IPlugin.cs
+++ src/Aitive.Framework.Plugins/IPlugin.cs
@@ -5,5 +5,12 @@ public interface IPlugin
     PluginId Id => Description.Id;
     PluginDescription Description { get; }
 
+    /// <summary>
+    /// Queries for a given interface as implemented by the plugin.
+    /// </summary>
+    /// <param name="interfaceType">The type of interface to request.</param>
+    /// <returns>
+    /// All implementations of that interface.
+    /// If this is disposable or async disposable it is the responsibilty of the caller to dispose the returned instances.</returns>
     IEnumerable<object> Query(Type interfaceType);
 }
