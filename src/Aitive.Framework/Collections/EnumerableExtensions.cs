e2329d90e1b3285faeaf7a2de5611b995fdc5880
Bernhard Glück <bernhard.glueck@aitive.at>
2026-02-17 15:21:52 +0100

    Adds extension methods for IEnumerable
    Adds extension methods to IEnumerable, including:
    - None/Empty: Checks if the IEnumerable is empty.
    - FirstOrOptional/LastOrOptional: Returns the first or last element matching a predicate, or Optional.None if no element is found.

--- src/Aitive.Framework/Collections/EnumerableExtensions.cs
+++ src/Aitive.Framework/Collections/EnumerableExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Aitive.Framework.Collections;
+﻿using Aitive.Framework.Functional;
+
+namespace Aitive.Framework.Collections;
 
 public static class EnumerableExtensions
 {
@@ -7,5 +9,81 @@ public static class EnumerableExtensions
         public bool None() => !enumerable.Any();
 
         public bool Empty() => !enumerable.Any();
+
+        public Optional<T> FirstOrOptional(Func<T, bool> predicate)
+        {
+            foreach (var item in enumerable)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
+            return Optional<T>.None;
+        }
+
+        public Optional<T> FirstOrOptional()
+        {
+            foreach (var item in enumerable)
+            {
+                return item;
+            }
+
+            return Optional<T>.None;
+        }
+
+        public Optional<T> LastOrOptional(Func<T, bool> predicate)
+        {
+            if (enumerable.TryGetNonEnumeratedCount(out var count))
+            {
+                var index = count - 1;
+
+                while (index >= 0)
+                {
+                    // ReSharper disable once PossibleMultipleEnumeration
+                    var element = enumerable.ElementAt(index);
+
+                    if (predicate(element))
+                    {
+                        return element;
+                    }
+
+                    index--;
+                }
+
+                return Optional<T>.None;
+            }
+
+            Optional<T> result = Optional<T>.None;
+            foreach (var item in enumerable)
+            {
+                if (predicate(item))
+                {
+                    result = item;
+                }
+            }
+
+            return result;
+        }
+
+        public Optional<T> LastOrOptional()
+        {
+            if (enumerable.TryGetNonEnumeratedCount(out var count))
+            {
+                if (count >= 0)
+                {
+                    return enumerable.ElementAt(count - 1);
+                }
+            }
+
+            Optional<T> result = Optional<T>.None;
+            foreach (var item in enumerable)
+            {
+                result = item;
+            }
+
+            return result;
+        }
     }
 }
