37a0d3d373ae7b2f46d2252de3af9ed53752eec6
Bernhard Glück <bernhard.glueck@aitive.at>
2026-03-06 00:52:51 +0100

    Implements YesSql integration and dependency injection extensions
    - Completes the `Aitive.Framework.YesSql` project by adding core integration logic.
    - Provides `IServiceCollection` extensions to configure and register the YesSql `IStore`.
    - Enables automatic registration of all `IIndexProvider` implementations resolved from the service provider.

---
+++ src/Aitive.Framework.YesSql/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YesSql;
+using YesSql.Indexes;
+
+namespace Aitive.Framework.YesSql;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public void AddYesSql()
+        {
+            services.AddSingleton<IStore>(sp =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+                var store = StoreFactory.Create(configuration);
+                var indices = sp.GetServices<IIndexProvider>();
+
+                store.RegisterIndexes(indices);
+
+                return store;
+            });
+        }
+    }
+}
