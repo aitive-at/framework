5683581ad5ef35a65fdb805fcc6357909faa97c4
Bernhard Glück <bernhard.glueck@aitive.at>
2026-03-12 12:41:58 +0100

    Adds plugin system integration for Marten
    - Implements `MartenPluginBindPointBuilder` to automatically register plugin assemblies with Marten's schema scanner.
    - Adds a `ConfigureMartenWithPlugins` extension method to streamline service registration, connection string setup, and plugin binding.

---
+++ src/Aitive.Framework.Marten/Plugins/MartenPluginBindPointBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Aitive.Framework.Plugins;
+using Aitive.Framework.Plugins.Binding;
+using Marten;
+
+namespace Aitive.Framework.Marten.Plugins;
+
+public sealed class MartenPluginBindPointBuilder : IPluginBindPointBuilder
+{
+    private readonly StoreOptions _storeOptions;
+
+    public MartenPluginBindPointBuilder(StoreOptions storeOptions)
+    {
+        _storeOptions = storeOptions;
+    }
+
+    public IPluginBindPoint Build(IReadOnlyList<IPlugin> plugins)
+    {
+        _storeOptions.AutoRegister(scanner =>
+        {
+            foreach (var assembly in plugins.SelectMany(p => p.Query<Assembly>()))
+            {
+                scanner.Assembly(assembly);
+            }
+        });
+
+        return new EmptyPluginBindPoint();
+    }
+}
---
+++ src/Aitive.Framework.Marten/Plugins/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Aitive.Framework.Configuration;
+using Aitive.Framework.Plugins;
+using Marten;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Aitive.Framework.Marten.Plugins;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public void ConfigureMartenWithPlugins(IPluginHost pluginHost, string connectionStringKey)
+        {
+            services.AddMarten(
+                (serviceProvider) =>
+                {
+                    var storeOptions = new StoreOptions();
+                    var configurations = serviceProvider.GetServices<
+                        IConfigureOptions<StoreOptions>
+                    >();
+                    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+                    var connectionString = configuration.GetRequiredConnectionString(
+                        connectionStringKey
+                    );
+
+                    storeOptions.Connection(connectionString);
+
+                    foreach (var configurationStep in configurations)
+                    {
+                        configurationStep.Configure(storeOptions);
+                    }
+
+                    _ = pluginHost.Bind(new MartenPluginBindPointBuilder(storeOptions));
+
+                    return storeOptions;
+                }
+            );
+        }
+    }
+}
