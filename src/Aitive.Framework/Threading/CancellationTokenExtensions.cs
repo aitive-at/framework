4f8c3bf52348853a028a9216d8e6d1ac1f14c507
Bernhard Glück <bernhard.glueck@aitive.at>
2026-03-28 02:00:46 +0100

    Adds extension method to asynchronously wait for CancellationToken signaling
    Introduces an `Await` method for `CancellationToken` that allows a task to wait until a token is cancelled. The method provides an option to either throw a `TaskCanceledException` or complete successfully when the cancellation signal is received.

---
+++ src/Aitive.Framework/Threading/CancellationTokenExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Aitive.Framework.Threading;
+
+public static class CancellationTokenExtensions
+{
+    extension(CancellationToken cancellationToken)
+    {
+        public async Task Await(bool throwOnCancellation = false)
+        {
+            var completionSource = new TaskCompletionSource();
+
+            var token = cancellationToken.Register(() =>
+            {
+                if (throwOnCancellation)
+                {
+                    completionSource.TrySetCanceled(cancellationToken);
+                }
+                else
+                {
+                    completionSource.TrySetResult();
+                }
+            });
+
+            try
+            {
+                await completionSource.Task;
+            }
+            finally
+            {
+                await token.DisposeAsync();
+            }
+        }
+    }
+}
