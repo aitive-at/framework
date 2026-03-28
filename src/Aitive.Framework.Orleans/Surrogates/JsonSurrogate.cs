7a8cdb0a7799623c2393a4bd37e4aa526d737ac1
Bernhard Glück <bernhard.glueck@aitive.at>
2026-03-28 02:25:13 +0100

    Adds generic JSON surrogate and converter for Orleans serialization
    Introduces `JsonSurrogate` and `JsonSurrogateConverter` to facilitate serializing types that implement `ITypedJsonDocument` as JSON strings within an Orleans cluster.

---
+++ src/Aitive.Framework.Orleans/Surrogates/JsonSurrogate.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Aitive.Framework.Json;
+using Aitive.Framework.Patterns;
+
+namespace Aitive.Framework.Orleans.Surrogates;
+
+[GenerateSerializer]
+public struct JsonSurrogate<T>
+    where T : ITypedJsonDocument<T>
+{
+    [Id(0)]
+    public string Json { get; set; }
+}
+
+public abstract class JsonSurrogateConverter<T> : IConverter<T, JsonSurrogate<T>>
+    where T : ITypedJsonDocument<T>
+{
+    public T ConvertFromSurrogate(in JsonSurrogate<T> surrogate)
+    {
+        return JsonSerializer.Deserialize<T>(
+                surrogate.Json,
+                Globals.Resolve<JsonSerializerOptions>()
+            ) ?? throw new InvalidOperationException();
+    }
+
+    public JsonSurrogate<T> ConvertToSurrogate(in T value)
+    {
+        return new JsonSurrogate<T>() { Json = value.ToJsonString() };
+    }
+}
