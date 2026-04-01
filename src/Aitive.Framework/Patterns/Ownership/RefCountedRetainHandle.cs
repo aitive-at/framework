1725e1209cfb18d91b90f651e917e24666652fe1
Bernhard Glück <bernhard.glueck@aitive.at>
2026-04-01 02:23:35 +0200

    Adds reference counting base classes and atomic long primitive
    Introduces `AtomicLong` for thread-safe numeric operations and provides `RefCountedRetainHandle` and `RefCountedCollection` as building blocks for managing shared resource lifetimes through reference counting.

---
+++ src/Aitive.Framework/Patterns/Ownership/RefCountedRetainHandle.cs
@@ -0,0 +1,29 @@
+﻿using Aitive.Framework.Threading;
+
+namespace Aitive.Framework.Patterns.Ownership;
+
+public abstract class RefCountedRetainHandle<TKey, TValue> : IRetainHandle<TKey, TValue>
+    where TKey : notnull
+{
+    private readonly AtomicLong _refCount;
+
+    protected RefCountedRetainHandle(TKey key, TValue value, AtomicLong refCount)
+    {
+        Key = key;
+        Value = value;
+        _refCount = refCount;
+    }
+
+    public long RefCount => _refCount.Value;
+    public TKey Key { get; }
+    public TValue Value { get; }
+
+    public async ValueTask DisposeAsync() { }
+}
+
+public abstract class RefCountedCollection<THandle, TKey, TValue>
+    where TKey : notnull
+    where THandle : IRetainHandle<TKey, TValue>
+{
+    protected abstract THandle CreateHandle(TKey key, TValue value);
+}
---
+++ src/Aitive.Framework/Threading/AtomicLong.cs
@@ -0,0 +1,43 @@
+﻿namespace Aitive.Framework.Threading;
+
+public sealed class AtomicLong
+{
+    private long _value;
+
+    public AtomicLong(long value = 0L)
+    {
+        _value = value;
+    }
+
+    public long Value
+    {
+        get => Volatile.Read(ref _value);
+        set => Set(value);
+    }
+
+    public long Increment(long increment = 1)
+    {
+        return Interlocked.Add(ref _value, increment);
+    }
+
+    public long Decrement(long decrement = 1)
+    {
+        return Interlocked.Add(ref _value, -decrement);
+    }
+
+    public long Set(long value)
+    {
+        return Interlocked.Exchange(ref _value, value);
+    }
+
+    /// <summary>
+    /// Compare Exchange
+    /// </summary>
+    /// <param name="expected">The value that is compared with and expected</param>
+    /// <param name="newValue">The value to set if the current value equals the expected value</param>
+    /// <returns>The original value before exchange</returns>
+    public long CompareExchange(long expected, long newValue)
+    {
+        return Interlocked.CompareExchange(ref _value, newValue, expected);
+    }
+}
